d9b06198bdd8e71729626b4476a7f9d51fc285d2
Hanan <hananjohn111@gmail.com>
2023-12-01 00:21:14 +0500

    Toolbar Added

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.test_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.test_app.databinding.ActivityMainBinding
 
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+
         binding.btnSecondActivity.setOnClickListener {
             val name = binding.etName.text.toString()
             val person = User(name, 21, "Staff Engineer")
@@ -26,6 +29,19 @@ class MainActivity : AppCompatActivity() {
                 startActivity(this)
             }
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.it_home -> Toast.makeText(this, "Home Button Clicked", Toast.LENGTH_SHORT).show()
+            R.id.it_list -> Toast.makeText(this, "List Button Clicked", Toast.LENGTH_SHORT).show()
+            R.id.it_grid -> Toast.makeText(this, "Grid Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
